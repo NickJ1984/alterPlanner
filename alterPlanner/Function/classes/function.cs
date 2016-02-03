9a7402604ec1ab5ac0f344c0e1c278ec50c9f02f
NickJ-test <nikolay.yudaev@gmail.com>
2016-02-03 15:11:01 +0300

    Function закомментировал

--- alterPlanner/Function/classes/function.cs
+++ alterPlanner/Function/classes/function.cs
@@ -9,19 +9,43 @@ using alter.types;
 
 namespace alter.Function.classes
 {
+    /// <summary>
+    /// Структура передающая основные характеристики функции зависимости от даты
+    /// </summary>
     public struct VectorF
     {
+        /// <summary>
+        /// Дата зависимости
+        /// </summary>
         public DateTime Date;
+        /// <summary>
+        /// Направление зависимости
+        /// </summary>
         public e_Direction Direction;
     }
+    /// <summary>
+    /// Класс реализующий временную зависимость (зависимость от даты)
+    /// </summary>
     public class function : IFunction
     {
         #region vars
+        /// <summary>
+        /// Дата зависимости
+        /// </summary>
         protected DateTime Date;
+        /// <summary>
+        /// Направление зависимости
+        /// </summary>
         protected e_Direction Direction;
+        /// <summary>
+        /// Делегат реализующий механику зависимости
+        /// </summary>
         protected Func<DateTime, DateTime> Function;
         #endregion
         #region props
+        /// <summary>
+        /// Свойство даты зависимости
+        /// </summary>
         public virtual DateTime date
         {
             get { return Date; }
@@ -35,6 +59,9 @@ namespace alter.Function.classes
                 }
             }
         }
+        /// <summary>
+        /// Свойство направление зависимости
+        /// </summary>
         public virtual e_Direction direction
         {
             get { return Direction; }
@@ -51,10 +78,21 @@ namespace alter.Function.classes
         }
         #endregion
         #region events
+        /// <summary>
+        /// Событие изменения даты зависимости
+        /// </summary>
         public event EventHandler<ea_ValueChange<DateTime>> event_DateChanged;
+        /// <summary>
+        /// Событие изменения направления зависимости
+        /// </summary>
         public event EventHandler<ea_ValueChange<e_Direction>> event_DirectionChanged;
         #endregion
         #region constructor
+        /// <summary>
+        /// Конструктор экземпляра класса зависимости от даты
+        /// </summary>
+        /// <param name="date">Дата зависимости</param>
+        /// <param name="direction">Направление зависимости</param>
         public function(DateTime date, e_Direction direction)
         {
             this.direction = direction;
@@ -63,11 +101,19 @@ namespace alter.Function.classes
         }
         #endregion
         #region handlers
+        /// <summary>
+        /// Метод запуска события <seealso cref="event_DateChanged"/>
+        /// </summary>
+        /// <param name="args">Аргументы события</param>
         protected void OnDateChange(ea_ValueChange<DateTime> args)
         {
             EventHandler<ea_ValueChange<DateTime>> handler = event_DateChanged;
             if (handler != null) handler(this, args);
         }
+        /// <summary>
+        /// Метод запуска события <seealso cref="event_DirectionChanged"/>
+        /// </summary>
+        /// <param name="args">Аргументы события</param>
         protected void OnDirectionChange(ea_ValueChange<e_Direction> args)
         {
             EventHandler<ea_ValueChange<e_Direction>> handler = event_DirectionChanged;
@@ -75,10 +121,20 @@ namespace alter.Function.classes
         }
         #endregion
         #region methods
+        /// <summary>
+        /// Метод реализующий механику зависимости
+        /// </summary>
+        /// <param name="date">Проверяемая дата</param>
+        /// <returns></returns>
         public DateTime CheckDate(DateTime date)
         {
             return Function(Date);
         }
+        /// <summary>
+        /// Метод получения нового делегата реализующего механику зависимости с направлением заданным параметром метода.
+        /// </summary>
+        /// <param name="direction">Направление зависимости получаемого делегата</param>
+        /// <returns>Делегат реализующий механику зависимости </returns>
         public Func<DateTime, DateTime, DateTime> GetFunctionDir(e_Direction direction)
         {
             switch (direction)
@@ -87,32 +143,51 @@ namespace alter.Function.classes
                 //case eDirection.RightMax:
                 case e_Direction.Fixed:
                     return new Func<DateTime, DateTime, DateTime>(
-                        (limit, Date) => Date = limit);
+                        (limit, date) => date = limit);
 
                 case e_Direction.Left:
                     return new Func<DateTime, DateTime, DateTime>(
-                    (limit, Date) => (Date > limit) ? limit : Date);
+                    (limit, date) => (date > limit) ? limit : date);
 
                 case e_Direction.Right:
                     return new Func<DateTime, DateTime, DateTime>(
-                    (limit, Date) => (Date < limit) ? limit : Date);
+                    (limit, date) => (date < limit) ? limit : date);
                 default:
                     throw new Exception("getSvcFunction: wrong direction value");
             }
         }
 
-
+        /// <summary>
+        /// Метод получения даты зависимости
+        /// </summary>
+        /// <returns>Дата зависимости</returns>
         public virtual DateTime GetDate()
         { return date; }
+        /// <summary>
+        /// Метод установки даты зависимости
+        /// </summary>
+        /// <param name="date">Дата зависимости</param>
         public void SetDate(DateTime date)
         { this.date = date; }
-
+        /// <summary>
+        /// Метод получения направления зависимости
+        /// </summary>
+        /// <returns>Направление зависимости</returns>
         public virtual e_Direction GetDirection()
         { return direction; }
+        /// <summary>
+        /// Метод получения направления зависимости
+        /// </summary>
+        /// <param name="direction">Направление зависимости</param>
         public void SetDirection(e_Direction direction)
         { this.direction = direction; }
         #endregion
         #region service
+        /// <summary>
+        /// Получение делегата реализации механизма зависимости основывающееся на внутренней дате зависимости
+        /// </summary>
+        /// <param name="direction">Направление зависимости</param>
+        /// <returns>Делегат реализации механизма зависимости основывающееся на внутренней дате зависимости</returns>
         protected Func<DateTime, DateTime> GetSvcFunction(e_Direction direction)
         {
             switch(direction)
